713593c7a1236bbc282a4c000f1669b5d4cf8bab
nakamari <yuuki.0200doglove@gmail.com>
2022-10-25 23:09:48 +0900

    refactor: warning修正

--- src/components/atoms/SelectBox/index.tsx
+++ src/components/atoms/SelectBox/index.tsx
@@ -10,12 +10,8 @@ export const SelectBox: FC<Props> = ({ selectMenu }) => {
   const { handleChange } = useHooks();
   return (
     <select css={styles.root} name="セレクトボックス" onChange={handleChange}>
-      <option selected={selectMenu === 'top'} value=".">
-        選択してください
-      </option>
-      <option selected={selectMenu === 'todo'} value="todo">
-        TODOリスト
-      </option>
+      <option value=".">選択してください</option>
+      <option value="todo">TODOリスト</option>
     </select>
   );
 };
