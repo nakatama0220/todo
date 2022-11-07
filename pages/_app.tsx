7652e0b038b7aa9ed8c5db5e86c5d2c5ed4fe874
nakamari <yuuki.0200doglove@gmail.com>
2022-11-07 17:03:31 +0900

    feat: sessionで切り替え実装

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,27 @@
 import { Global } from '@emotion/react';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 import { global } from '../styles/global';
 import { reset } from '../styles/reset';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session;
+}>) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
   return (
-    <>
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}>
       <Global styles={global} />
       <Global styles={reset} />
       <Component {...pageProps} />
-    </>
+    </SessionContextProvider>
   );
 }
 
