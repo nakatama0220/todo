3ffc8ad2567f50630de72fd9f7c74dcfce2f9677
nakamari <yuuki.0200doglove@gmail.com>
2022-11-07 17:09:14 +0900

    feat: ログアウトボタン実装追加

--- src/components/organisms/Top/index.tsx
+++ src/components/organisms/Top/index.tsx
@@ -1,11 +1,19 @@
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
 import type { FC } from 'react';
 import { SelectBox } from '../../atoms/SelectBox';
 import { styles } from './styles';
 
 export const Top: FC = () => {
+  const supabase = useSupabaseClient();
+
   return (
     <div css={styles.root}>
-      <h1 css={styles.title}>トップページ</h1>
+      <div css={styles.wrapper}>
+        <h1 css={styles.title}>トップページ</h1>
+        <button css={styles.button} type="button" onClick={() => supabase.auth.signOut()}>
+          ログアウト
+        </button>
+      </div>
       <SelectBox selectMenu="top" />
     </div>
   );
