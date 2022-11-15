32e74552197e9ddcd95435692b44760229c289d3
nakamari <yuuki.0200doglove@gmail.com>
2022-11-15 09:59:11 +0900

    feat: トータル時間の実装追加

--- src/components/organisms/Attendance/hooks.ts
+++ src/components/organisms/Attendance/hooks.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import { useRouter } from 'next/router';
 import { useState, useCallback, useEffect } from 'react';
 import supabase from '../../../../utils/supabase';
@@ -20,6 +21,20 @@ export const useHooks = (): Hooks => {
   const router = useRouter();
   const [list, setList] = useState<Item[]>([]);
 
+  const getTotalScore = useCallback(
+    (attendanceTime: string, breakingTime: number, workedTime: string) => {
+      if (attendanceTime.length === 0 || workedTime.length === 0) return 'None';
+      const minute = dayjs(workedTime).diff(dayjs(attendanceTime), 'm');
+      const hours = dayjs(workedTime).diff(dayjs(attendanceTime), 'h');
+      const resultMinute = minute - breakingTime;
+      if (hours > 0) {
+        return `${hours}時${resultMinute}分`;
+      }
+      return `${resultMinute}分`;
+    },
+    [],
+  );
+
   const fetch = useCallback(async () => {
     const datas = (await supabase.from('attendance').select('*')).data;
     if (!datas) return;
@@ -28,10 +43,10 @@ export const useHooks = (): Hooks => {
       attendanceTime: changeTime(item.attendance_time, 'YYYY年MM月DD日HH時mm分'),
       breakingTime: `${item.breaking_time}分`,
       workedTime: changeTime(item.worked_time, 'YYYY年MM月DD日HH時mm分'),
-      totalScore: '',
+      totalScore: getTotalScore(item.attendance_time, item.breaking_time, item.worked_time),
     }));
     setList(result);
-  }, []);
+  }, [getTotalScore]);
 
   const handleTopPage = useCallback(() => {
     router.push('.');
