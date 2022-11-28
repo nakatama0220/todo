0753c1d95054e6fb2444b273331cd0ed1876d8cd
nakamari <yuuki.0200doglove@gmail.com>
2022-11-29 08:46:21 +0900

    fix: URL直で叩けてしまうため修正

--- pages/attendance.tsx
+++ pages/attendance.tsx
@@ -1,12 +1,23 @@
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
+import { Auth, ThemeSupa } from '@supabase/auth-ui-react';
 import type { NextPage } from 'next';
 import { Attendance as AttendancePage } from '../src/components/organisms/Attendance';
 import { Layout } from '../src/components/template/Layout';
 
 const Attendance: NextPage = () => {
+  const session = useSession();
+  const supabase = useSupabaseClient();
+
   return (
-    <Layout>
-      <AttendancePage />
-    </Layout>
+    <>
+      {!session ? (
+        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+      ) : (
+        <Layout>
+          <AttendancePage />
+        </Layout>
+      )}
+    </>
   );
 };
 
