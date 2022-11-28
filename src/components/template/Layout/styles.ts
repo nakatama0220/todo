283708d24ec4bed997a1e94956f4520affc46229
nakamari <yuuki.0200doglove@gmail.com>
2022-11-29 08:56:13 +0900

    style: style修正

--- src/components/template/Layout/styles.ts
+++ src/components/template/Layout/styles.ts
@@ -2,18 +2,8 @@ import { css } from '@emotion/react';
 
 export const styles = {
   main: css`
-    min-height: 100vh;
-    padding: 4rem 0;
-    flex: 1;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
+    height: 100vh;
+    padding-top: 50px;
     background-color: #f1ebd7;
-    gap: 30px;
-    min-width: 1350px;
-  `,
-  title: css`
-    font-size: 3rem;
-    color: #9acd32;
   `,
 };
