b799c0e142e0838101eec380b5d1f95adb31407e
nakamari <yuuki.0200doglove@gmail.com>
2022-11-28 12:15:51 +0900

    style: style変更

--- src/components/molecules/AttendanceButtonWrapper/styles.ts
+++ src/components/molecules/AttendanceButtonWrapper/styles.ts
@@ -1,24 +1,6 @@
 import { css } from '@emotion/react';
 
 export const styles = {
-  root: css`
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    align-items: center;
-  `,
-  title: css`
-    font-size: 3rem;
-    color: #9acd32;
-  `,
-  wrapper: css`
-    display: flex;
-    gap: 20px;
-  `,
-  button: css`
-    color: red;
-    background-color: white;
-  `,
   buttonWrapper: css`
     display: flex;
     gap: 15px;
