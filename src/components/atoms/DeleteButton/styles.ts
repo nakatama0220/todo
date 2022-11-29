97ebf5a0a8f34b4d6511b405b6a21a59dcb612b2
nakamari <yuuki.0200doglove@gmail.com>
2022-11-29 10:03:28 +0900

    feat: DeleteButtonコンポーネント作成

---
+++ src/components/atoms/DeleteButton/styles.ts
@@ -0,0 +1,10 @@
+import { css } from '@emotion/react';
+
+export const styles = {
+  root: css`
+    background-color: #ff0000;
+    padding: 5px;
+    border-radius: 5px;
+    color: #fff;
+  `,
+};
